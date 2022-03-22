861f0054d13174c317e6878043d223bd53fb3543
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-03-23 01:20:15 +0600

    done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,31 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import "./Product.css";
 
 const Product = (props) => {
   const { name, img, seller, price, ratings } = props.product;
+
   return (
     <div className="product">
       <img src={img} alt="" />
+      <div className="product-info">
+        <p className="product-name">{name}</p>
+        <p>Price: {price}</p>
+        <p>
+          <small>Seller: {seller}</small>
+        </p>
+        <p>
+          <small>Ratings: {ratings}</small>
+        </p>
+      </div>
+      <button
+        onClick={() => props.handleClick(props.product)}
+        className="btn-cart"
+      >
+        <p>Add to cart</p>
+        <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+      </button>
     </div>
   );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,21 +4,33 @@ import "./Shop.css";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     fetch("products.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+  const handleClick = (product) => {
+    console.log(product);
+    const newCart = [...cart, product];
+    setCart(newCart);
+  };
 
   return (
     <div className="shop-container">
       <div className="product-container">
         {products.map((product) => (
-          <Product key={product.id} product={product}></Product>
+          <Product
+            key={product.id}
+            product={product}
+            handleClick={handleClick}
+          ></Product>
         ))}
       </div>
-      <div className="card-container"></div>
+      <div className="card-container">
+        <p>Selected Item : {cart.length}</p>
+      </div>
     </div>
   );
 };
