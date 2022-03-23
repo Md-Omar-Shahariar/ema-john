3c6db25a8e73c3d735142b5f3e9c966caef0d11e
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-03-23 18:38:58 +0600

    cart info

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cart = (props) => {
+  return (
+    <div>
+      <h4>Hello from cart {props.cart.length} </h4>
+    </div>
+  );
+};
+
+export default Cart;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
 
@@ -28,8 +29,8 @@ const Shop = () => {
           ></Product>
         ))}
       </div>
-      <div className="card-container">
-        <p>Selected Item : {cart.length}</p>
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
       </div>
     </div>
   );
